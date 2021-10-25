9ffebfd0e26042480d39144353808690cf333314
Jim Milks <jrmilks@yahoo.com>
2021-10-25 12:29:22 -0400

    Additions and updates
    Updates to the Attendance report files and the baseline data set, plus the inclusion of a Shiny survey app

---
+++ app.R
@@ -0,0 +1,40 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinysurveys)
+
+KetSDA_demograpics <- data.frame(question = c("What is your age?", "What is your gender?", "What is your gender?", "Which service do you normally attend?", "Which service do you normally attend?"),
+                                 option = c("", "Female", "Male", "Ascent", "Sanctuary"),
+                                 input_type = c("numeric", "mc", "mc", "mc", "mc"),
+                                 input_id = c("Age", "gender", "gender", "service", "service"),
+                                 dependence = c(NA, NA, NA, NA, NA),
+                                 dependence_value = c(NA, NA, NA, NA, NA),
+                                 required = c(TRUE, TRUE, TRUE, TRUE, TRUE)
+)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+        surveyOutput(df = KetSDA_demograpics,
+                     survey_title = "Kettering SDA Church demographics",
+                     survey_description = "Welcome! We would like to know a bit more about our congregation. The information you provide will be used to create a population pyramid for our congregation to help us identify current and future needs. Please fill this form out for each member of your immediate household, reloading it each time. Thank you for participating!")
+        
+)
+
+server <- function(input, output, session) {
+        renderSurvey()
+        
+        observeEvent(input$submit, {
+                showModal(modalDialog(
+                        title = "Thank you!"
+                ))
+        })
+}
+
+shinyApp(ui, server)
